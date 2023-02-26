0072e4755ebbd315a75ffa83a5b398cef2c5dce1
mirandaasral <mia323@lehigh.edu>
2023-02-26 14:54:22 -0500

    more changes

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -52,32 +52,69 @@ function App() {
   return (
     <div className="App">
       <div className="name-header">
-        <h1>Miranda Asral</h1>
-        <p className="right-items">Resume</p>
+        <h1
+          className="m-0"
+          style={{
+            paddingLeft: ".5em",
+          }}
+        >
+          Miranda Asral
+        </h1>
+        <p className="right-items m-0">Resume</p>
       </div>
-      <div className="row portfolio-list">
-        {/* row is a bootstrap class (it is just a row, good for mobile view stuff) */}
-        {pieces.length &&
-          // .map is an array function that returns the elements in the return section for every element
-          // in the specific array
-          pieces.map((piece, i) => {
-            return (
-              <div className="col-6 m-0 list-item">
-                {/* col-6 makes the div the size of half of the width of the row */}
-                {/* m-0 removes all the default margins of the col-6 (m-0 means no margins anywhere) */}
-                <img
-                  src={piece.listImg}
-                  alt=""
-                  key={"img_" + i}
-                  className="list-image"
-                  onClick={(event) => {
-                    setSelectedPiece(piece);
-                  }}
-                />
-              </div>
-            );
-          })}
-      </div>
+      {/* full view of one piece */}
+      {selectedPiece && (
+        <div
+          className="piece-view"
+          style={{
+            display: "flex",
+          }}
+        >
+          <div className="piece-description">
+            <h2>{selectedPiece.name}</h2>
+            <p>{selectedPiece.description}</p>
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedPiece(null);
+              }}
+              className="piece-back-link"
+            >
+              &larr; Back
+            </a>
+          </div>
+          <div className="piece-image">
+            <img alt="" src={selectedPiece.fullImg} />
+          </div>
+        </div>
+      )}
+      {/* list view of all pieces */}
+      {!selectedPiece && (
+        <div className="row portfolio-list">
+          {/* row is a bootstrap class (it is just a row, good for mobile view stuff) */}
+          {pieces.length &&
+            // .map is an array function that returns the elements in the return section for every element
+            // in the specific array
+            pieces.map((piece, i) => {
+              return (
+                <div className="col-6 m-0 list-item" key={"div_" + i}>
+                  {/* col-6 makes the div the size of half of the width of the row */}
+                  {/* m-0 removes all the default margins of the col-6 (m-0 means no margins anywhere) */}
+                  <img
+                    src={piece.listImg}
+                    alt=""
+                    key={"img_" + i}
+                    className="list-image"
+                    onClick={(event) => {
+                      event.preventDefault();
+                      setSelectedPiece(piece);
+                    }}
+                  />
+                </div>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 }
